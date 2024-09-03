ca81613b79523bba118e3a8e1ea63d153d14ceb3
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2024-09-03 16:04:53 +0330

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -5,7 +5,6 @@ from contextlib import suppress
 from pathlib import Path
 from typing import Any
 
-#import matplotlib.pyplot as plt
 import numpy as np
 import shinyswatch
 from astral import LocationInfo
